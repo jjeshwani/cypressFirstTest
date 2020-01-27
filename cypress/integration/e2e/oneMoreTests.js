239aa421e8b75d4d71b909507a28773e130a5cc6
jaikishan <jackiejeswani@gmail.com>
2020-01-27 16:39:11 +0100

    - Updates Navigated to present page clicked ß button and checked ß is present in the text box

--- cypress/integration/e2e/oneMoreTests.js
+++ cypress/integration/e2e/oneMoreTests.js
@@ -12,9 +12,23 @@ describe('Actions', () => {
                 cy.get('.choosebox').select('Past Perfect Conjugation');
                 cy.get('form > div:nth-child(2) .button').click()
             });
-            it('should navigate to `Verbs/conjugation/present/` form', () => {
+            it('should navigate to `Verbs/conjugation/past-perfect/` form', () => {
                 cy.url().should('include', 'past-perfect');
                 cy.get('#content h1').should('contains.text', 'Past Perfect Conjugation');
+            });
+        });
+    });
+
+    describe('given `Present Tense Conjugation` page', () => {
+        before( () =>{
+            cy.visit('verbs/conjugation/present/')
+        });
+        describe('when user clicks on `ß` letter in front of `ich`', () => {
+            before( () =>{
+                cy.get('[onclick="add_special1(\'ß\')"]').click()
+            });
+            it('should display `ß` letter inside input filed', () => {
+                cy.get('[name*="input1"]').should('have.value', 'ß')
             });
         });
     });
