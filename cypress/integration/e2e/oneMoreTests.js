8cf395711ccee9fc84c0b59185419ff170129467
jaikishan <jackiejeswani@gmail.com>
2020-01-27 16:10:58 +0100

    - Updates Completed dropdown selection url assert new page element check

--- cypress/integration/e2e/oneMoreTests.js
+++ cypress/integration/e2e/oneMoreTests.js
@@ -1,25 +1,20 @@
-context('Actions', () => {
-    beforeEach(() => {
+describe('Actions', () => {
+    before(() => {
         cy.visit('/')
     });
 
     describe('given `Conjugation` page', () => {
-        before( () =>{
+        before(() => {
             cy.visit('verbs/conjugation/')
         });
         describe('when user selects `Past Perfect Conjugation` in drop down and clicks `Start` button', () => {
-            before( () =>{
-
-                cy.get('.choosebox').select('Past Perfect Conjugation')
-
-                cy.get('form > div:nth-child(2) .button').click({force: true})
-
+            before(() => {
+                cy.get('.choosebox').select('Past Perfect Conjugation');
+                cy.get('form > div:nth-child(2) .button').click()
             });
             it('should navigate to `Verbs/conjugation/present/` form', () => {
-                // TODO add assertion to verify that you were redirected to `verbs/conjugation/present/` page
-                console.log(cy.url())
-
-                // TODO add assertion that the form with Conjugation is present
+                cy.url().should('include', 'past-perfect');
+                cy.get('#content h1').should('contains.text', 'Past Perfect Conjugation');
             });
         });
     });
