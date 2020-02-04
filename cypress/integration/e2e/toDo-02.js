14c7a16874f556027df3bee0ffd7a8e23c666592
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2020-02-04 10:48:27 +0100

    - add toDo-02.js

---
+++ cypress/integration/e2e/toDo-02.js
@@ -0,0 +1,17 @@
+describe('German.net UI POM improvements', () => {
+
+    describe('given `Reading` page', () => {
+        before(() => {
+            cy.visit('reading/');
+        });
+        it('should verify presence of navigation section', () => {
+            //TODO add assertion for
+        });
+        it('should verify presence of text section', () => {
+            //TODO add assertion for
+        });
+        it('should verify presence of table results section', () => {
+            //TODO add assertion for
+        });
+    });
+});
